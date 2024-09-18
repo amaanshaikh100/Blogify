05880d36b2692181bd36d3e9d1b9227319798bfd
YOUR_USERNAME <amaanshaikh59442@outlook.com>
2024-09-18 18:08:06 +0530

    Add basic error handler for dev and prod environments

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const morgan = require("morgan");
 const blogRouter = require("./routes/blogRoutes");
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controller/errorController");
 
 const app = express();
 
@@ -11,5 +13,11 @@ if (process.env.NODE_ENV === "development") {
 }
 
 app.use("/api/v1/blogs", blogRouter);
+
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404), 404);
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
