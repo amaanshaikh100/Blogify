68163a426ca4c31442e4672f6dac34648b678d53
YOUR_USERNAME <amaanshaikh59442@outlook.com>
2024-09-19 00:00:02 +0530

    Add security against cross site scripting and brute attacks

--- app.js
+++ app.js
@@ -2,16 +2,34 @@ const express = require("express");
 const morgan = require("morgan");
 const blogRouter = require("./routes/blogRoutes");
 const userRouter = require("./routes/userRoutes");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controller/errorController");
 
 const app = express();
 
-app.use(express.json());
+app.use(helmet());
+
+app.use(express.json({ limit: "10kb" }));
+
+app.use(mongoSanitize());
+
+app.use(xss());
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 100,
+  message: "Too many request from this IP address. Please try again later.",
+});
+
+app.use("/api", limiter);
 
 app.use("/api/v1/blogs", blogRouter);
 app.use("/api/v1/users", userRouter);
