cbf265458b7292fc062481a9a34116bb01425f4d
YOUR_USERNAME <amaanshaikh59442@outlook.com>
2024-09-18 23:05:10 +0530

    Implmented login and protect routes

--- controller/authController.js
+++ controller/authController.js
@@ -1,5 +1,6 @@
 const jwt = require("jsonwebtoken");
 const User = require("./../models/userModel");
+const { promisify } = require("util");
 const AppError = require("./../utils/appError");
 const catchAsync = require("./../utils/catchAsync");
 
@@ -45,4 +46,29 @@ exports.logIn = catchAsync(async (req, res, next) => {
     status: "success",
     token,
   });
+});
+
+exports.protect = catchAsync(async (req, res, next) => {
+  let token = "";
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  }
+
+  if (!token) {
+    return next(
+      new AppError("You are not logged in! Please log in to get access.", 401)
+    );
+  }
+
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+  const currentUser = await User.findById(decoded.id);
+
+  req.user = currentUser;
+
+  next();
 });
--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const blogController = require("./../controller/blogsController");
+const authController = require("./../controller/authController");
 const router = express.Router();
 
 router
   .route("/")
-  .get(blogController.getAllBlogs)
+  .get(authController.protect, blogController.getAllBlogs)
   .post(blogController.createBlog);
 
 router
